165d4da50843c6e754a165fddf608be5c91222eb
imanrahil <irahil@agilorg.com>
2018-04-12 12:49:26 +0100

    [DEL] pha_partner_parc : suppresion module

--- pha_partner_parc/models/partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = ['res.partner']
-
-    parc_materials = fields.Text(string='Parc Matériels')
