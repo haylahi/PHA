be2c751c15869a86744112b2b993da0717bbd6e1
imanrahil <irahil@agilorg.com>
2018-04-13 15:34:57 +0100

    [ADD] l10n_fr_intrastat_product: Ajout module

--- l10n_fr_intrastat_product/__manifest__.py
+++ l10n_fr_intrastat_product/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'DEB agilorgg',
+    'name': 'DEB ',
     'version': '10.0.1.0.0',
     'category': 'Localisation/Report Intrastat',
     'license': 'AGPL-3',
