fd2d77f6c5747eba131dc3d3333917ee08e92365
imanrahil <irahil@agilorg.com>
2018-04-13 14:23:38 +0100

    [ADD] l10n_fr_intrastat_product : Ajout module

--- l10n_fr_intrastat_product/__manifest__.py
+++ l10n_fr_intrastat_product/__manifest__.py
@@ -1,54 +1,34 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Report intrastat product module for OpenERP
-#    Copyright (C) 2010-2014 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# © 2010-2017 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'France Intrastat Product agilorg',
-    'version': '1.0',
+    'name': 'DEB agilorgg',
+    'version': '10.0.1.0.0',
     'category': 'Localisation/Report Intrastat',
     'license': 'AGPL-3',
-    'summary': 'Module for Intrastat product reporting (DEB) for France',
-    'description': """This module adds support for the "Déclaration d'Echange de Biens" (DEB).
-
-More information about the DEB is available on this official web page : http://www.douane.gouv.fr/page.asp?id=322
-
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
-    """,
+    'summary': "DEB (Déclaration d'Échange de Biens) for France",
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
-    'depends': ['intrastat_product', 'sale_stock', 'purchase', 'l10n_fr_siret'],
+    'depends': [
+        'intrastat_product',
+        'l10n_fr_siret',
+        'l10n_fr_department',
+        ],
     'data': [
         'security/intrastat_product_security.xml',
         'security/ir.model.access.csv',
+        'views/intrastat_product.xml',
+        'data/intrastat_transaction.xml',
+        'views/intrastat_transaction.xml',
+        'views/intrastat_unit.xml',
         'data/intrastat_product_reminder.xml',
-        'data/intrastat_type_data.xml',
-        'views/intrastat_product_view.xml',
-        'views/intrastat_type_view.xml',
-        'views/company_view.xml',
-        'views/partner_view.xml',
-        'views/product_view.xml',
-        'views/stock_view.xml',
-        'views/invoice_view.xml',
+        'views/account_config_settings.xml',
+        'views/partner.xml',
+        'views/product.xml',
     ],
+    'post_init_hook': 'set_fr_company_intrastat',
     'demo': ['demo/intrastat_demo.xml'],
     'installable': True,
     'application': True,
--- l10n_fr_intrastat_product/models/__init__.py
+++ l10n_fr_intrastat_product/models/__init__.py
@@ -1,31 +1,9 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Report intrastat product module for Odoo
-#    Copyright (C) 2010-2014 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
 
-from . import intrastat_type
+from . import intrastat_transaction
 from . import partner
-from . import product
+from . import intrastat_unit
 from . import stock
+from . import account_config_settings
 from . import company
-from . import invoice
-from . import sale
-from . import purchase
 from . import intrastat_product
