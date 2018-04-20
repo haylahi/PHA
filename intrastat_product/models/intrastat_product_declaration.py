1f431112f6abc324bf0f9b3ff3e521b01ad89dcc
imanrahil <irahil@agilorg.com>
2018-04-20 10:22:22 +0100

    [IMP] intrastat_product :Récupérer le pays de de destination/Provenance au lieu du pays d'origine de l'article sur les lignes de déclaration

--- intrastat_product/models/intrastat_product_declaration.py
+++ intrastat_product/models/intrastat_product_declaration.py
@@ -458,6 +458,23 @@ class IntrastatProductDeclaration(models.Model):
         """ placeholder for localization modules """
         pass
 
+    def _gather_origin_country (self, inv_line):
+         if self.type == 'arrivals':
+             inv_line.product_origin_country_id = self._get_partner_country(inv_line)
+         if self.type == 'dispatches':
+             inv_line.product_origin_country_id = self.env.user.company_id.country_id
+
+         return inv_line.product_origin_country_id
+
+    # def _get_partner_country(self, inv_line):
+    #     country = inv_line.invoice_id.src_dest_country_id \
+    #               or inv_line.invoice_id.partner_id.country_id
+    #     if not country.intrastat:
+    #         country = False
+    #     elif country == self.company_id.country_id:
+    #         country = False
+    #     return country
+
     def _gather_invoices(self):
 
         lines = []
@@ -493,7 +510,6 @@ class IntrastatProductDeclaration(models.Model):
                         'of invoice %s. Reason: qty = 0'
                         % (inv_line.name, inv_line.quantity, invoice.number))
                     continue
-
                 partner_country = self._get_partner_country(inv_line)
                 if not partner_country:
                     _logger.info(
@@ -542,7 +558,7 @@ class IntrastatProductDeclaration(models.Model):
                 amount_company_currency = self._get_amount(inv_line)
                 total_inv_product_cc += amount_company_currency
 
-                product_origin_country = self._get_product_origin_country(
+                product_origin_country = self._gather_origin_country(
                     inv_line)
 
                 region = self._get_region(inv_line)
