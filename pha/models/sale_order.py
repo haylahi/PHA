afe122216a6859d28507cb207ce42fff6a290644
Ayoub Zahid <azahid@agilorg.com>
2018-05-04 01:30:34 +0100

    [WIP] pha: templates commandes/factures (Work In Progress).

--- pha/models/sale_order.py
+++ pha/models/sale_order.py
@@ -4,7 +4,6 @@ import math
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.one
     def max_line(self,index,max):
         max_line = int(math.ceil(index / max)) * max
         return max_line
