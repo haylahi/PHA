eb47b6e7c04014442b7c1a84c288a26a8c6ecbe5
ben mbark youssef <benmbarkyoussef@gmail.com>
2018-10-18 10:02:29 +0100

    [DEL]-supprimer fichiers non necessaires

--- pha_update_pricelist/__manifest__.py
+++ pha_update_pricelist/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'PHA',
+    'name': 'PHA update pricelist',
     'version': '0.1',
     'category': 'Product Management',
 
@@ -10,13 +10,9 @@
     'website': "http://www.cadrinsitu.com",
 
 
-    'depends': ['base','sale','sale_stock','sale_order_dates','pha_partner_fax',
-                'stock','account','purchase','ci_account_desc','stock_location',
-                'mrp_repair','website_sale'],
+    'depends': ['base','sale'],
     'data': [
        'views/product_pricelist_view.xml',
-        'views/mrp_repair_views.xml',
-        'reports/mrp_repair.xml',    
     ],
 
     'installable': True,
--- pha_update_pricelist/models/__init__.py
+++ pha_update_pricelist/models/__init__.py
@@ -2,4 +2,3 @@
 
 from . import sale_order
 from . import product_pricelist
-from . import mrp_repair
