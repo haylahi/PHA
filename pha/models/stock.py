7d24c36de49225ed46107f4aaeece5548f73970a
Ayoub Zahid <azahid@agilorg.com>
2018-10-26 16:01:23 +0100

    [IMP] PHA : sur la vue liste détails des mouvemments de stock, afficher le partenaire en fonction du type d'operation (livraison => client ; Reception => fournisseur)

--- pha/models/stock.py
+++ pha/models/stock.py
@@ -1,8 +1,16 @@
 from odoo import models, fields, api
-
+import logging
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    partner_id = fields.Many2one(related='move_id.sale_line_id.order_id.partner_id', store=True)
+    def _get_partner(self):
+        for rec in self:
+            if rec.picking_type_id.code == 'outgoing':
+                rec.partner_id = rec.move_id.sale_line_id.order_id.partner_id.id
+
+            if rec.picking_type_id.code == 'incoming':
+                rec.partner_id = rec.move_id.purchase_line_id.order_id.partner_id.id
+
+    partner_id = fields.Many2one('res.partner',string='Partenaire', compute='_get_partner')
     picking_type_id = fields.Many2one(related='picking_id.picking_type_id', store=True)
