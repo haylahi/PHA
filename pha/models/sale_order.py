f5dc9a906cf3b7fd620e383ac5fa1c10ea792d36
Ayoub Zahid <azahid@agilorg.com>
2018-10-31 13:44:30 +0100

    [IMP] PHA : afficher la date de commande dans la vue détail des ventes (lignes de commande)

--- pha/models/sale_order.py
+++ pha/models/sale_order.py
@@ -9,6 +9,7 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    date_order = fields.Datetime(related='order_id.date_order', string='Date')
     @api.multi
     @api.onchange('product_id')
     def product_id_change(self):
